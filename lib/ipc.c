809950af249e0e708015677ae6af19174fef2328
Thomas Hybel <tehybel@gmail.com>
2017-05-17 14:56:48 +0200

    implemented ipc_recv and ipc_send in the user-mode C library

--- lib/ipc.c
+++ lib/ipc.c
@@ -14,32 +14,58 @@
 //	they're nonnull) and return the error.
 // Otherwise, return the value sent by the sender
 //
-// Hint:
-//   Use 'thisenv' to discover the value and who sent it.
-//   If 'pg' is null, pass sys_ipc_recv a value that it will understand
-//   as meaning "no page".  (Zero is not the right value, since that's
-//   a perfectly valid place to map a page.)
 int32_t
 ipc_recv(envid_t *from_env_store, void *pg, int *perm_store)
 {
-	// LAB 4: Your code here.
-	panic("ipc_recv not implemented");
-	return 0;
+	int32_t error;
+
+	// if 'pg' is null, pass sys_ipc_recv a value which means "no page"
+	if (pg == NULL)
+		pg = (void *) -1;
+
+	error = sys_ipc_recv(pg);
+
+	if (error) {
+		// syscall failed
+		if (from_env_store)
+			*from_env_store = 0;
+		if (perm_store)
+			*perm_store = 0;
+		return error;
+	}
+
+	// syscall succeeded
+	if (from_env_store)
+		*from_env_store = thisenv->env_ipc_from;
+	if (perm_store)
+		*perm_store = thisenv->env_ipc_perm;
+
+	return thisenv->env_ipc_value;
 }
 
 // Send 'val' (and 'pg' with 'perm', if 'pg' is nonnull) to 'toenv'.
 // This function keeps trying until it succeeds.
 // It should panic() on any error other than -E_IPC_NOT_RECV.
-//
-// Hint:
-//   Use sys_yield() to be CPU-friendly.
-//   If 'pg' is null, pass sys_ipc_try_send a value that it will understand
-//   as meaning "no page".  (Zero is not the right value.)
 void
 ipc_send(envid_t to_env, uint32_t val, void *pg, int perm)
 {
-	// LAB 4: Your code here.
-	panic("ipc_send not implemented");
+	int error;
+
+	// if 'pg' is null, pass a value which means "no page"
+	if (pg == NULL) {
+		pg = (void *) -1;
+		perm = 0;
+	}
+
+	while (1) {
+		error = sys_ipc_try_send(to_env, val, pg, perm);
+		if (error == 0)
+			break;
+		else if (error == -E_IPC_NOT_RECV)
+			sys_yield();
+		else
+			panic("sys_ipc_try_send returned %e", error);
+	}
 }
 
 // Find the first environment of the given type.  We'll use this to
