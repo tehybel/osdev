a4bdf0c8e210a03d0c3934d72cdea7926615a9b6
immerse <thismailisdispensable@gmail.com>
2017-10-19 12:15:06 +0200

    rewrote the assembly for do_init_graphics to not use a hardcoded mode, but instead enumerate and dynamically choose one.

--- kern/graphics.c
+++ kern/graphics.c
@@ -5,7 +5,8 @@
 #include <kern/graphics.h>
 
 extern void *realmode_gdt;
-extern int do_init_graphics();
+extern int _get_video_mode();
+extern int _set_video_mode();
 
 void io_event_put(struct io_event *event) {
 	assert (io_events_queue_cursize >= 0);
@@ -33,38 +34,61 @@ bool graphics_enabled() {
 }
 
 void init_graphics() {
+	unsigned int mode_offset;
+	int done;
 	
+	have_graphics = 0;
+
 	if (!graphics_enabled()) {
-		cprintf("Will not initialize graphics.\n");
-		have_graphics = 0;
+		cprintf("Graphics are not enabled.\n");
 		return;
 	}
 	
 	// since we're switching to real mode, we need to do so from code which
 	// resides at a place which is 1:1 mapped, which should currently be true
 	// for the first 1MB of code.
-	memcpy((void *) 0x8000, do_init_graphics, 0x1000);
+	memcpy((void *) 0x8000, _get_video_mode, 0x1000);
 	memcpy((void *) 0x9000, &realmode_gdt, 0x1000);
+	int (*get_video_mode)(int) = (void *) 0x8000;
 
-	cprintf("Setting video mode..\n");
+	// enumerate the different video modes
+	for (mode_offset = 0, done = 0; get_video_mode(mode_offset); mode_offset += 2) {
 
-	int (*fptr)() = (void *) 0x8000;
-	int res = fptr();
+		// if get_video_mode succeeded, a mode info struct now resides at 0xd000
+		mode_info = *(struct vbe_mode_info *) 0xd000;
 
-	if (!res) {
-		have_graphics = 0;
-		cprintf("Failed to set a video mode.\n");
-		return;
+		// cprintf("Video mode %d: %dx%dx%d\n", mode_offset, mode_info.width,
+		// 		mode_info.height, mode_info.bpp);
+
+		if (mode_info.width == GRAPHICS_WIDTH && 
+			mode_info.height == GRAPHICS_HEIGHT && 
+			mode_info.bpp == GRAPHICS_BPP) {
+
+			have_graphics = 1;
+			break;
+		}
 	}
 
-	// if do_init_graphics succeeded, a mode info struct now resides at 0xd000
-	mode_info = *(struct vbe_mode_info *) 0xd000;
+	if (!have_graphics) {
+		cprintf("Failed to find a valid video mode.\n");
+		return;
+	} 
 
-	have_graphics = 1;
-	lfb_size = (mode_info.width + mode_info.pitch) * mode_info.height *
-		mode_info.bpp / 8;
-
-	cprintf("Managed to set video mode!\n");
+	cprintf("Found a valid video mode:\n");
 	print_mode_info(&mode_info);
+
+	memcpy((void *) 0x8000, _set_video_mode, 0x1000);
+	memcpy((void *) 0x9000, &realmode_gdt, 0x1000);
+	int (*set_video_mode)(int) = (void *) 0x8000;
+
+	if (!set_video_mode(mode_offset)) {
+		cprintf("Could not set the video mode.\n");
+		have_graphics = 0;
+		return;
+	}
+	cprintf("Successfully set the video mode.\n");
+
+	lfb_size = (mode_info.width + mode_info.pitch) * mode_info.height *
+				mode_info.bpp / 8;
 }
 
